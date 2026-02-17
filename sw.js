9c5e032b1eb45bf62f7c3a95be5883c97fa623bd
CharPLop <pierangelo.lopresti@gmail.com>
2026-02-17 15:17:59 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = "template-social-v1";
+const ASSETS = [
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
+  "./icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok && e.request.url.startsWith("http")) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
